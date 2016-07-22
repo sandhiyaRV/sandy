e34a0f34d3e19b9fa21836ac9bd869ce303c0554
sandhiyaRV <sandhishines@gmail.com>
2016-07-22 15:06:59 +0530

    Create PositiveOrNegative.java

---
+++ PositiveOrNegative.java
@@ -0,0 +1,15 @@
+class PositiveOrNegative
+{
+public static void main(String[] args)
+{
+int num=123;
+if(num>0)
+{
+System.out.println(POSITIVE);
+}
+else
+{
+System.out.println(NEGATIVE);
+}
+}
+}
