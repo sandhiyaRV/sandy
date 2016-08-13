9a6bdf4cc939fd7a1990d39dfcb7f9baade4dbed
sandhiyaRV <sandhishines@gmail.com>
2016-08-13 11:45:31 +0530

    Create concat.java

---
+++ concat.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Concat {
+public static void main(String[] args) { 
+		Scanner sn=new Scanner(System.in);
+		System.out.println("Enter the String1 : ");
+		String s1=sn.nextLine();
+		System.out.println("Enter the string2 : ");
+		String s2=sn.nextLine();
+		System.out.println(s1+ " "+s2);
+		
+
+		
+			}
+
+}
